fe585178e6139937eac6184aab5373a98f5141eb
oligauld <oliver.gauld.14@live.ucl.ac.uk>
2015-04-01 19:17:33 +0100

    update

--- matlab/mutual_info_neuron/MI_calculation.m
+++ matlab/mutual_info_neuron/MI_calculation.m
@@ -1,8 +1,6 @@
 function [MI, Htotal, final_Hnoise] = MI_calculation(all_data_repeats,wordlength);
 
-% MI = Htotal - Hnoise
 global nreps
-nreps = 50
 
 % organise the data
 for loop = 1:size(all_data_repeats,1)
@@ -41,15 +39,15 @@ end
 %% reads 'words' over all response repetitions
 for x = 1:nreps
     for i = 1:length(binned_data(1,:))-(wordlength-1)
-        noisewords{x,i} = num2str(binned_data(x,i:i+wordlength-1));
+        responsewords{x,i} = num2str(binned_data(x,i:i+wordlength-1));
     end
 end
  
 %% calculate Htotal (calculates for each repetition then averages)
 for x = 1:nreps % loop through all repetitions
     
-     words = noisewords(x,:);
-     response_probs = sort(countmember(unique(words),words),'descend')/length(noisewords); % vector of the word probabilities present in the column
+     words = responsewords(x,:);
+     response_probs = sort(countmember(unique(words),words),'descend')/length(responsewords); % vector of the word probabilities present in the column
 
      for i = 1:length(response_probs)        
          H(i) = response_probs(i)*log2(response_probs(i));
@@ -63,11 +61,11 @@ end
 Htotal = mean(entropy_rate); % takes the mean of all 50 repetitions
 
 %% Hnoise, takes column responses for each t step then calculates entropy and averages
-for i = 1:length(noisewords)
+for i = 1:length(responsewords)
 
-    column = noisewords(:,i);
-    column_words = unique(column)'; % finds unique 'words' in the column
-    column_words_freq = countmember(unique(column_words),column); % counts unique 'words'
+    noise_column = responsewords(:,i);
+    column_words = unique(noise_column)'; % finds unique 'words' in the column
+    column_words_freq = countmember(unique(column_words),noise_column); % counts unique 'words'
     Hnoise_probs = sort(column_words_freq,'descend')/nreps; % vector of all words probabilities
 
     for ii = 1:length(Hnoise_probs)
